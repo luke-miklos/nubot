f0dc86004c089b0f2323fa97603ff10381791eb6
luke.b.miklos@gmail.com <luke.b.miklos@gmail.com@31f56fa5-24b1-03bf-fb14-41868dd8b7a4>
2013-04-25 02:52:00 +0000

    added cluster manager

---
+++ trunk/NubotBWSAL/Source/ClusterManager.h
@@ -0,0 +1,38 @@
+
+#include <BWAPI.h>
+#include <BWSAL.h>
+#include <vector>
+
+
+class ClusterManager
+{
+public:
+
+   typedef struct {
+      int left;
+      int right;
+      int distance;  //max = 2147483647
+   } ClusterNode;
+
+   struct NodeCompare {
+       bool operator ()(ClusterNode const& a, ClusterNode const& b) const
+       {
+          return (a.distance < b.distance);
+       }
+   };
+
+   static std::vector<BWSAL::UnitGroup> GetClustersByCount(BWSAL::UnitGroup& group, int numClusters);
+   static std::vector<BWSAL::UnitGroup> GetClustersByDistance(BWSAL::UnitGroup& group, int distThreshold);
+
+private:
+
+   static std::vector<ClusterNode>      GenerateDendrogram(BWSAL::UnitGroup& group);
+
+   static std::vector<BWSAL::UnitGroup> SplitDendrogramIntoClusters(BWSAL::UnitGroup& group, 
+                                                                    std::vector<ClusterNode> dendrogram,
+                                                                    int numClusters);
+
+   static int                             mLastN;
+   static std::vector< std::vector<int> > mDistMatrix;
+};
+
